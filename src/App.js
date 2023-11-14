1d8d6dbe51c27de13fbe934d5dfc9fea82dbbb97
Dmitrii Karpenko <ixdixdi@gmail.com>
2023-11-14 17:39:01 +0500

    Master record

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const Div = styled.div`
+	text-align: center;
+`;
+
+export const App = () => {
+	return (
+		<Div>
+			<div>123</div>
+			<i className="fa fa-calendar"></i>
+		</Div>
+	);
+};
